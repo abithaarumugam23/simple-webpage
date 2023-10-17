88fe28022bb2a017c164990c9f4d8001669b7504
abithaarumugam23 <147526865+abithaarumugam23@users.noreply.github.com>
2023-10-17 19:51:08 +0530

    Add files via upload

---
+++ sample_webpage/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sample_webpage.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult welcome()
+        {
+            string name = Request.Form["name"];
+            int age = int.Parse(Request.Form["age"]);
+            if (age > 18)
+            {
+                ViewBag.msg = "welcome !! " + name + ", You are eligible to visit this page";
+            }
+            else
+            {
+                ViewBag.msg = "welcome !! " + name + ", You are not eligible to visit this page";
+            }
+            Session["name"] = name;
+            Session["age"] = age;
+            return View();
+
+        }
+    }
+}
